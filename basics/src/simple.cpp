3ec7983509edbb87d06338e52aaf7a3f852938cb
Jose Mayoral <jocamaba1989@gmail.com>
2023-03-24 18:47:51 -0600

    basics

--- basics/src/simple.cpp
+++ basics/src/simple.cpp
@@ -1,9 +1,9 @@
-#include<rclcpp/rclcpp.h>
+#include<rclcpp/rclcpp.hpp>
 
 int main(int argc, char** argv){
     rclcpp::init(argc, argv);
 
-    auto node = rrclcpp::Node::make_shared("simple_node");
+    auto node = rclcpp::Node::make_shared("simple_node");
     rclcpp::spin(node);
 
     rclcpp::shutdown();
