874971954eb65782a8b15017cb0a819fbb80439b
Jose <jocamaba1989@gmail.com>
2023-08-14 17:42:08 -0500

    config file path

--- br2_basics/launch/param_node_v2_launch.py
+++ br2_basics/launch/param_node_v2_launch.py
@@ -1,14 +1,23 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
-    params_file = "install/br2_basics/share/br2_basics/config/params.yaml"
+    print(FindPackageShare("br2_basics"))
+
+    config = os.path.join(
+        get_package_share_directory('br2_basics'),
+        'config',
+        'params.yaml'
+    )
+    print (config)
     params_cmd = Node(
         package="br2_basics",
         executable="param_reader",
         output="screen",
-        parameters= [params_file]
+        parameters= [config]
     )
 
     ld = LaunchDescription()
