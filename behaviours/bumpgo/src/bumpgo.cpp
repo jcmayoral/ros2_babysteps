61181000ca8a1a4a15faf8390c63f7283c6e5be6
Jose <jocamaba1989@gmail.com>
2023-10-25 12:56:49 -0500

    corrections

--- behaviours/bumpgo/src/bumpgo.cpp
+++ behaviours/bumpgo/src/bumpgo.cpp
@@ -1,22 +1,21 @@
-#include <bumpgo/bumpogo.hpp>
+#include <bumpgo/bumpgo.h>
 
 BumpGo::BumpGo() : Node("bump_go")
 {
-    laser_sub_ = create_subscription<sensor_msgs::msg::LaserScan>("input_scan", rclcpp::SensorDataQos(),
-        std::bind(&BumpGo::scan_callback, this, _1));
-    vel_pub_ = create_publisher<geometry_msgs::msg::Twist>("output_vel", 10);
-    timer_ = create_wall_time(50ms, std::bind(&BumpGo::control_cycle, this));
-
+    laser_sub_ = create_subscription<sensor_msgs::msg::LaserScan>("input_scan", rclcpp::SensorDataQoS(),
+                                                                  std::bind(&BumpGo::scan_callback, this, std::placeholders::_1));
+    twist_pub_ = create_publisher<geometry_msgs::msg::Twist>("output_vel", 10);
+    timer_ = create_wall_timer(50ms, std::bind(&BumpGo::control_cycle, this));
 }
 
-BumpGo::~BumpGo(){
-    
+BumpGo::~BumpGo()
+{
 }
 
-void BumpGo::scan_callback(cost sensor_msgs::msg::LaserScan::UniquePtr msg){
-
+void BumpGo::scan_callback(const sensor_msgs::msg::LaserScan::UniquePtr msg)
+{
 }
 
-void BumpGo::control_cycle(){
-    
+void BumpGo::control_cycle()
+{
 }
