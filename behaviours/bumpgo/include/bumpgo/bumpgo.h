fc1923865e84c0b164669b8d4833d6b0cf1022c2
Jose <jocamaba1989@gmail.com>
2023-10-25 14:20:38 -0500

    update

--- behaviours/bumpgo/include/bumpgo/bumpgo.h
+++ behaviours/bumpgo/include/bumpgo/bumpgo.h
@@ -8,6 +8,12 @@ class BumpGo : public rclcpp::Node {
     private:
         void scan_callback(sensor_msgs::msg::LaserScan::UniquePtr msg);
         void control_cycle();
+        void go_state(int new_state);
+        bool check_forward_2_back();
+        bool check_forward_2_stop();
+        bool check_stop_2_forward();
+        bool check_back_2_turn();
+        bool check_turn_2_forward();
 
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
         rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
@@ -17,10 +23,18 @@ class BumpGo : public rclcpp::Node {
 
         static const int FORWARD = 0;
         static const int BACK = 1;
-        static const int LEFT = 2;
-        static const int RIGHT = 3;
+        static const int TURN = 2;
+        static const int STOP = 3;
+
+        const rclcpp::Duration TURNING_TIME {2s};
+        const rclcpp::Duration BACKING_TIME {2s};
+        const rclcpp::Duration SCAN_TIMEOUT {1s};
+    
+        static constexpr float SPEED_LINEAR = 0.3f;
+        static constexpr float SPEED_ANGULAR = 0.3f;
+        static constexpr float OBSTACLE_DISTANCE = 1.0f;
         int state_;
-        rclcpp::Time state_ts;
+        rclcpp::Time state_ts_;
         
     public:
         BumpGo();
--- behaviours/bumpgo/src/bumpgo.cpp
+++ behaviours/bumpgo/src/bumpgo.cpp
@@ -6,6 +6,7 @@ BumpGo::BumpGo() : Node("bump_go"), state_{FORWARD}
                                                                   std::bind(&BumpGo::scan_callback, this, std::placeholders::_1));
     twist_pub_ = create_publisher<geometry_msgs::msg::Twist>("output_vel", 10);
     timer_ = create_wall_timer(50ms, std::bind(&BumpGo::control_cycle, this));
+    state_ts_ = now();
 }
 
 BumpGo::~BumpGo()
@@ -19,7 +20,80 @@ void BumpGo::scan_callback(sensor_msgs::msg::LaserScan::UniquePtr msg)
 
 void BumpGo::control_cycle()
 {
-    if(last_scan_ == nullptr)
+    if (last_scan_ == nullptr)
         return;
-    twist_pub_->publish(geometry_msgs::msg::Twist());
+
+    geometry_msgs::msg::Twist out_vel;
+
+    switch (state_)
+    {
+    case FORWARD:
+        out_vel.linear.x = SPEED_LINEAR;
+    
+        if (check_forward_2_stop())
+            go_state(STOP);
+        if (check_forward_2_back())
+            go_state(BACK);
+        break;
+
+    case BACK:
+      out_vel.linear.x = -SPEED_LINEAR;
+
+      if (check_back_2_turn()) {
+        go_state(TURN);
+      }
+
+      break;
+    case TURN:
+      out_vel.angular.z = SPEED_ANGULAR;
+
+      if (check_turn_2_forward()) {
+        go_state(FORWARD);
+      }
+
+      break;
+    case STOP:
+      if (check_stop_2_forward()) {
+        go_state(FORWARD);
+      }
+      break;
+    }
+
+    twist_pub_->publish(out_vel);
+}
+
+void BumpGo::go_state(int new_state)
+{
+    state_ = new_state;
+    state_ts_ = now();
+}
+
+
+bool BumpGo::check_forward_2_back()
+{
+    size_t pos = last_scan_ -> ranges.size() /2;
+    return last_scan_ -> ranges[pos] < OBSTACLE_DISTANCE;
+}
+
+bool BumpGo::check_forward_2_stop()
+{
+    auto elapsed = now() - rclcpp::Time(last_scan_->header.stamp);
+    return elapsed > SCAN_TIMEOUT;
+}
+
+bool BumpGo::check_stop_2_forward()
+{
+  auto elapsed = now() - rclcpp::Time(last_scan_->header.stamp);
+  return elapsed < SCAN_TIMEOUT;
+}
+
+bool BumpGo::check_back_2_turn()
+{
+    return (now() - state_ts_) > BACKING_TIME;
+
+}
+
+bool BumpGo::check_turn_2_forward()
+{
+  return (now() - state_ts_) > TURNING_TIME;
 }
